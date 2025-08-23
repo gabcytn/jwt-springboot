87794de816e041217d8dcba140bb5d265d74d4e9
gabcytn <cayetanogabriel03@gmail.com>
2025-08-23 11:34:52 +0800

    test: invalid device name on refresh token

--- src/test/java/com/gabcytyn/jwtdemo/Feature/RegistrationAndLoginTest.java
+++ src/test/java/com/gabcytyn/jwtdemo/Feature/RegistrationAndLoginTest.java
@@ -12,6 +12,7 @@ import com.gabcytyn.jwtdemo.DTO.RegisterUserDto;
 import com.github.javafaker.Faker;
 import jakarta.servlet.http.Cookie;
 import java.util.Objects;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,52 +20,95 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class RegistrationAndLoginTest {
   private final Faker faker = new Faker();
+  private final String cookieName = "X-REFRESH-TOKEN";
   @Autowired private MockMvc mockMvc;
   @Autowired private ObjectMapper objectMapper;
 
+  private String email;
+  private String fullName;
+  private String password;
+  private String deviceName;
+
+  @BeforeEach
+  public void init() {
+    this.email = getRandomEmail();
+    this.fullName = getRandomFullName();
+    this.password = getRandomPassword();
+    this.deviceName = getRandomWord();
+  }
+
   @Test
   public void successfulRegistrationAndLogin() throws Exception {
-    String email = getRandomEmail(),
-        fullName = getRandomFullName(),
-        password = getRandomPassword(),
-        deviceName = getRandomWord();
-    RegisterUserDto register = new RegisterUserDto(email, fullName, password);
-    mockMvc
-        .perform(
-            post("/auth/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(register)))
-        .andDo(print())
-        .andExpect(status().isCreated());
+    RegisterUserDto registerReqBody = new RegisterUserDto(email, fullName, password);
+    ResultActions registerActions = this.register(objectMapper.writeValueAsString(registerReqBody));
 
-    LoginUserDto login = new LoginUserDto(email, password, deviceName);
+    registerActions.andExpect(status().isCreated());
+
+    LoginUserDto loginReqBody = new LoginUserDto(email, password, deviceName);
+    ResultActions loginActions = this.login(objectMapper.writeValueAsString(loginReqBody));
+
     MvcResult result =
-        mockMvc
-            .perform(
-                post("/auth/login")
-                    .contentType(MediaType.APPLICATION_JSON)
-                    .content(objectMapper.writeValueAsString(login)))
-            .andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(cookie().exists("X-REFRESH-TOKEN"))
-            .andReturn();
+        loginActions.andExpect(status().isOk()).andExpect(cookie().exists(cookieName)).andReturn();
 
-    Cookie refreshToken = Objects.requireNonNull(result.getResponse().getCookie("X-REFRESH-TOKEN"));
+    Cookie refreshToken = this.getRefreshTokenCookie(result);
 
     RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest(deviceName);
-    mockMvc
+    ResultActions refreshTokenActions =
+        this.refreshToken(objectMapper.writeValueAsString(refreshTokenRequest), refreshToken);
+    refreshTokenActions.andExpect(status().isOk());
+  }
+
+  @Test
+  public void invalidDeviceName() throws Exception {
+    RegisterUserDto registerReqBody = new RegisterUserDto(email, fullName, password);
+    ResultActions registerActions = this.register(objectMapper.writeValueAsString(registerReqBody));
+
+    registerActions.andExpect(status().isCreated());
+
+    LoginUserDto loginReqBody = new LoginUserDto(email, password, deviceName);
+    ResultActions loginActions = this.login(objectMapper.writeValueAsString(loginReqBody));
+
+    MvcResult result =
+        loginActions.andExpect(status().isOk()).andExpect(cookie().exists(cookieName)).andReturn();
+
+    Cookie refreshToken = this.getRefreshTokenCookie(result);
+
+    RefreshTokenRequest refreshTokenRequest = new RefreshTokenRequest(this.getRandomWord());
+    ResultActions refreshTokenActions =
+        this.refreshToken(objectMapper.writeValueAsString(refreshTokenRequest), refreshToken);
+    refreshTokenActions.andExpect(status().isBadRequest());
+  }
+
+  private ResultActions register(String reqBody) throws Exception {
+    return mockMvc
+        .perform(post("/auth/register").contentType(MediaType.APPLICATION_JSON).content(reqBody))
+        .andDo(print());
+  }
+
+  private ResultActions login(String reqBody) throws Exception {
+    return mockMvc
+        .perform(post("/auth/login").contentType(MediaType.APPLICATION_JSON).content(reqBody))
+        .andDo(print());
+  }
+
+  private ResultActions refreshToken(String reqBody, Cookie cookie) throws Exception {
+    return mockMvc
         .perform(
             post("/auth/refresh-token")
-                .cookie(refreshToken)
+                .cookie(cookie)
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(refreshTokenRequest)))
-        .andDo(print())
-        .andExpect(status().isOk());
+                .content(reqBody))
+        .andDo(print());
+  }
+
+  private Cookie getRefreshTokenCookie(MvcResult result) {
+    return Objects.requireNonNull(result.getResponse().getCookie(cookieName));
   }
 
   private String getRandomEmail() {
