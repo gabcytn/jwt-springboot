44a427e582808ecf8abd57cca087eed9e8806f76
gabcytn <cayetanogabriel03@gmail.com>
2025-08-24 12:59:56 +0800

    test: new jwt request + rotate refresh token

---
+++ src/test/java/com/gabcytyn/jwtdemo/Unit/NewJwtServiceTests.java
@@ -0,0 +1,79 @@
+package com.gabcytyn.jwtdemo.Unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+import com.gabcytyn.jwtdemo.DTO.LoginResponseDto;
+import com.gabcytyn.jwtdemo.DTO.RefreshTokenValidatorDto;
+import com.gabcytyn.jwtdemo.Exception.RefreshTokenException;
+import com.gabcytyn.jwtdemo.Service.AuthenticationService;
+import com.gabcytyn.jwtdemo.Service.CachingService;
+import com.gabcytyn.jwtdemo.Service.JwtService;
+import com.github.javafaker.Faker;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class NewJwtServiceTests {
+  private final Faker faker = new Faker();
+  @Mock private CachingService cachingService;
+  @Mock private JwtService jwtService;
+  @InjectMocks private AuthenticationService authenticationService;
+  private String oldRefreshToken;
+  private String generatedRefreshToken;
+  private String deviceName;
+  private String generatedJwt;
+
+  @BeforeEach
+  public void init() {
+    this.oldRefreshToken = faker.internet().uuid();
+    this.generatedRefreshToken = faker.internet().uuid();
+    this.generatedJwt = faker.internet().uuid();
+    this.deviceName = faker.app().name() + faker.name().name();
+  }
+
+  @Test
+  public void testRefreshTokenHappyFlow() {
+    RefreshTokenValidatorDto validatorDto =
+        new RefreshTokenValidatorDto(faker.internet().emailAddress(), this.deviceName);
+
+    when(cachingService.getRefreshTokenValidator(this.oldRefreshToken)).thenReturn(validatorDto);
+    when(jwtService.generateToken(validatorDto.email())).thenReturn(this.generatedJwt);
+    when(jwtService.generateRefreshToken()).thenReturn(this.generatedRefreshToken);
+
+    LoginResponseDto response = authenticationService.newJwt(this.oldRefreshToken, this.deviceName);
+
+    assertEquals(this.generatedJwt, response.getToken());
+
+    verify(cachingService, times(1)).deleteRefreshToken(this.oldRefreshToken);
+    verify(jwtService, times(1)).generateRefreshToken();
+    verify(cachingService, times(1)).saveRefreshToken(this.generatedRefreshToken, validatorDto);
+  }
+
+  @Test
+  public void testNullRefreshTokenValidator() {
+    when(cachingService.getRefreshTokenValidator(anyString())).thenReturn(null);
+
+    assertThrows(
+        RefreshTokenException.class,
+        () -> authenticationService.newJwt(this.oldRefreshToken, this.deviceName));
+  }
+
+  @Test
+  public void testMismatchDeviceName() {
+    RefreshTokenValidatorDto validatorDto =
+        new RefreshTokenValidatorDto(faker.internet().emailAddress(), this.deviceName);
+
+    when(cachingService.getRefreshTokenValidator(this.oldRefreshToken)).thenReturn(validatorDto);
+
+    assertThrows(
+        RefreshTokenException.class,
+        () -> authenticationService.newJwt(this.oldRefreshToken, faker.internet().uuid()));
+  }
+}
